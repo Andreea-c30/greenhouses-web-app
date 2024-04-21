fd217a29af55cfe2e01d269b344e15989bbadff4
project <vladislav.frunzze@gmail.com>
2024-04-21 17:55:04 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function app() {
+  return (
+    <div>
+      <h3>Hello midterms my old friends</h3>
+
+      <h4>
+        In this laboratory work, I will implement an app which will help to<br/>
+        manage your greenhouses. You can add your greenhouse in the app, choose<br/>
+        its name, location, and maybe some other properties. Then, you will see<br/>
+        some general data about each greenhouse like temperature, humidity, yield etc.
+       </h4>
+    </div>
+  );
+}
+
+export default app;
